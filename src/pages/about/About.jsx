7af6b5cec2bd94adf6210e34053b037e585fb303
ZoyaZubair <z.zoya@gmail.com>
2024-06-30 11:36:52 +0530

    initial commit

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-content">
+        <h2>About Us</h2>
+        <p>
+        Welcome to EduNexus – where education meets innovation. At EduNexus, we are dedicated to transforming the way you learn by providing an immersive and interactive educational experience. Our platform connects learners with top-tier educators and cutting-edge resources, making quality education accessible to everyone, everywhere.
+
+        Our mission is to democratize education by leveraging technology to bridge the gap between learners and educators. We believe that education is a fundamental right, and we are committed to providing high-quality learning experiences that are affordable, flexible, and tailored to individual needs.
+
+        We envision a world where every learner has access to the best educational resources, regardless of their geographical location or economic background. By fostering a global community of learners and educators, we aim to inspire and empower individuals to achieve their full potential.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
