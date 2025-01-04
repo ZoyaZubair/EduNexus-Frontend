b8b64e846e597fdb83c9420513f91ed407c364e0
ZoyaZubair <z.zoya@gmail.com>
2025-01-04 16:43:44 +0530

    Vercel change and logo fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = ({ isAuth }) => {
   return (
     <header>
-      <div className="logo">EduNexus</div>
+      <div className="logo"><Link to={"/"}>EduNexus</Link></div>
 
       <div className="link">
         <Link to={"/"}>Home</Link>
